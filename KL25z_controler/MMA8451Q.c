0534fe583e589403d374e360411359e46c1614d9
gruszor <48853308+gruszor@users.noreply.github.com>
2020-01-13 15:04:38 +0100

    basic things needed to run

--- KL25z_controler/MMA8451Q.c
+++ KL25z_controler/MMA8451Q.c
@@ -1,38 +1,46 @@
 #include "MMA8451Q.h"
 #include "UART_0.h" 
 
-void ACCEL_init(void){
-	I2C_init();
-	
-	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;				//Turn on PortA for MMA interrupt
-	PORTA -> PCR[14] |= PORT_PCR_MUX(1)				//
-										| PORT_PCR_ISF_MASK			//Clear interrupt falg
-										| PORT_PCR_IRQC(0xA);		//Config for fallin edge
-	
-	NVIC_ClearPendingIRQ(I2C0_IRQn);				/* Clear NVIC any pending interrupts on PORTA14 */
-	NVIC_EnableIRQ(I2C0_IRQn);							/* Enable NVIC interrupts source for PORTA14 */
-	NVIC_SetPriority (I2C0_IRQn, 0);				/* PORTA14 interrupt priority level  */ 
-	
-	uint8_t reg_val = 0;
-	
-	I2C_write(MMA_addr, CTRL_REG2, 0x40);
-	while(reg_val){
-		reg_val = I2C_read(CTRL_REG2, CTRL_REG2) & 0x40;
-	}
-	I2C_write(MMA_addr, XYZ_DATA_CFG, 0x00);
-	I2C_write(MMA_addr, CTRL_REG2, 0x02);
-	I2C_write(MMA_addr, CTRL_REG2, 0x3D);
-	
-	I2C_write(MMA_addr, CTRL_REG3, 0x40);
-	I2C_write(MMA_addr, CTRL_REG4, 0x20);
-	I2C_write(MMA_addr, CTRL_REG5, 0x20);
-	I2C_write(MMA_addr, ASLP_COUNT_REG, 0x03);
-	I2C_write(MMA_addr, CTRL_REG2, 0x1C);
-	I2C_write(MMA_addr, CTRL_REG1, 0x61);
+uint8_t AccData[6];
 
-}
+void ACCEL_init(void){
+    I2C_write(MMA_addr, CTRL_REG1, 0x01);
+    
+    
+    
+    /*
+    I2C_init();
+    
+    SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK;              //Turn on PortA for MMA interrupt
+    PORTA -> PCR[14] |= PORT_PCR_MUX(1)             //
+                                        | PORT_PCR_ISF_MASK         //Clear interrupt falg
+                                        | PORT_PCR_IRQC(0xB);       //Config for fallin edge
+    
+    NVIC_ClearPendingIRQ(I2C0_IRQn);                /* Clear NVIC any pending interrupts on PORTA14 */
+    //NVIC_EnableIRQ(I2C0_IRQn);                          /* Enable NVIC interrupts source for PORTA14 */
+    //NVIC_SetPriority (I2C0_IRQn, 0);                /* PORTA14 interrupt priority level  */ 
+    
+/*
+    I2C_write(MMA_addr, XYZ_DATA_CFG, 0x00); // max 2g
+    I2C_write(MMA_addr, CTRL_REG2, 0x02); // high resolution enabled (oversampling)
+    I2C_write(MMA_addr, CTRL_REG1, 0x3D);//   1,56hz,lownoise,,active
+    
+    I2C_write(MMA_addr, CTRL_REG3, 0x40);// fifo
+    I2C_write(MMA_addr, CTRL_REG4, 0x20);// fifo interrupt enable
+    I2C_write(MMA_addr, CTRL_REG5, 0x20);//  Interrupt is routed to INT1 pin ???
+    I2C_write(MMA_addr, ASLP_COUNT_REG, 0x03); //time ???
+    I2C_write(MMA_addr, CTRL_REG2, 0x1C); // sleep mode: low power, auto sleep enabled
+    I2C_write(MMA_addr, CTRL_REG1, 0x61); // 1,56hz in auto sleep mode //It is important to note that when the device is auto-sleep mode, the system ODR and the data rate for all the system functional
+                                                                       //blocks are overridden by the data rate set by the ASLP_RATE field.
+    */
+    
+    /*I2C_write(MMA_addr, CTRL_REG1, 0x00);             // Standby mode to allow writing to the offset registers 
+    I2C_write(MMA_addr, CTRL_REG3, 0x00);             // Push-pull, active low interrupt
+    I2C_write(MMA_addr, CTRL_REG4, 0x21);             // Enable DRDY interrupt
+    I2C_write(MMA_addr, CTRL_REG5, 0x21);             // DRDY interrupt routed to INT1 - PTA14
+    I2C_write(MMA_addr, CTRL_REG1, 0x3D);             // ODR = 1.56Hz, Reduced noise, Active mode */
+    }
 
 uint16_t MMA_DATA(uint8_t* DATA_S){
-	I2C_multiRegRead(MMA_addr, OUT_X_MSB_REG, 6, DATA_S);
+    I2C_multiRegRead(MMA_addr, OUT_X_MSB_REG, 6, DATA_S);
 }
-
--- KL25z_controler/main.c
+++ KL25z_controler/main.c
@@ -1,52 +1,110 @@
+  
 #include "MKL25Z4.h"                    // Device header
 #include "I2C.h" 
 #include "UART_0.h" 
 #include "leds.h"
 #include "MMA8451Q.h"
 
-uint8_t DataReady = 1;
-uint8_t RAW_DATA[6];
+int delay = 0xaffff;// delay value
+volatile uint8_t DataReady = 1;
+volatile uint8_t RAW_DATA[6];
 uint16_t A_DATA = 0;
+
 void main(void){
-	UART_init(9600);
-	print_char('U');
-	I2C_init();
-	print_char('F');
-	ACCEL_init(); 
-	print_word("Init done \n\r");
-	A_DATA = I2C_read(MMA_addr, WHO_AM_I);
-	print_word(A_DATA);
-	print_word("I2C 1st read done \n\r");
-	//I2C_multiRegRead(MMA_addr, OUT_X_MSB_REG, 2, RAW_DATA);
-//	print_word(RAW_DATA[0]);
-	print_word("I2C 2nd read done \n\r");
-	
-	while(1){
-		if(DataReady){
-			DataReady = 0;
-			MMA_DATA(RAW_DATA);
-			
-			A_DATA = (RAW_DATA[0]<<8 | RAW_DATA[1]>>2);
-			print_word("X-axis: ");
-			print_num(A_DATA);
-			print_char('\n');
-			A_DATA = (RAW_DATA[2]<<8 | RAW_DATA[3]>>2);
-			print_word("Y-axis: ");
-			print_num(A_DATA);
-			print_char('\n');
-			A_DATA = (RAW_DATA[4]<<8 | RAW_DATA[5]>>2);
-			print_word("Z-axis: ");
-			print_num(A_DATA);
-			print_char('\n');
-		}
-	}
-	
+    
+    I2C_init();
+    ACCEL_init();
+    
+    while(1)
+        {
+            print_word("\r\nwait data");
+            MMA_DATA(RAW_DATA);
+            RAW_DATA[0] = I2C_read(MMA_addr, OUT_X_MSB_REG);
+            RAW_DATA[1] = I2C_read(MMA_addr, OUT_X_LSB_REG);
+            RAW_DATA[2] = I2C_read(MMA_addr, OUT_Y_MSB_REG);
+            RAW_DATA[3] = I2C_read(MMA_addr, OUT_Y_LSB_REG);
+            RAW_DATA[4] = I2C_read(MMA_addr, OUT_Z_MSB_REG);
+            RAW_DATA[5] = I2C_read(MMA_addr, OUT_Z_LSB_REG);
+            print_word("\r\nready data");
+            
+            A_DATA = (uint16_t)(RAW_DATA[0]<<8) | (uint16_t)(RAW_DATA[1]>>2);
+            print_word("X-axis: ");
+            print_num(A_DATA);
+            print_char('\n');
+            A_DATA = (uint16_t)(RAW_DATA[2]<<8) | (uint16_t)(RAW_DATA[3]>>2);
+            print_word("\rY-axis: ");
+            print_num(A_DATA);
+            print_char('\n');
+            A_DATA = (uint16_t)(RAW_DATA[4]<<8) | (uint16_t)(RAW_DATA[5]>>2);
+            print_word("\rZ-axis: ");
+            print_num(A_DATA);
+            print_char('\n');
+            
+            while(delay)
+                {
+                    --delay;
+                }
+        }
+    
+    
+    /*
+    UART_init(9600);
+    print_char('U');
+    I2C_init();
+    print_char('F');
+    ACCEL_init(); 
+    print_word("\n\rInit Uart, I2C, Accel done \n\r");
+    A_DATA = I2C_read(MMA_addr, WHO_AM_I);
+    print_word("Who I am: \n\r");
+    print_num(A_DATA);
+    print_word("\n\rI2C 1st read done \n\r");
+    //I2C_multiRegRead(MMA_addr, OUT_X_MSB_REG, 2, RAW_DATA);
+//  print_word(RAW_DATA[0]);
+    print_word("I2C 2nd read done \n\r");
+    
+    while(1){
+        print_word("\r\nPrint data: \n\r");
+        if(DataReady){
+            DataReady = 0;
+            print_word("\r\nwait data");
+            MMA_DATA(RAW_DATA);
+//          RAW_DATA[0] = I2C_read(MMA_addr, OUT_X_MSB_REG);
+//          RAW_DATA[1] = I2C_read(MMA_addr, OUT_X_LSB_REG);
+//          RAW_DATA[2] = I2C_read(MMA_addr, OUT_Y_MSB_REG);
+//          RAW_DATA[3] = I2C_read(MMA_addr, OUT_Y_LSB_REG);
+//          RAW_DATA[4] = I2C_read(MMA_addr, OUT_Z_MSB_REG);
+//          RAW_DATA[5] = I2C_read(MMA_addr, OUT_Z_LSB_REG);
+            print_word("\r\nready data");
+            
+            A_DATA = (uint16_t)(RAW_DATA[0]<<8) | (uint16_t)(RAW_DATA[1]>>2);
+            print_word("X-axis: ");
+            print_num(A_DATA);
+            print_char('\n');
+            A_DATA = (uint16_t)(RAW_DATA[2]<<8) | (uint16_t)(RAW_DATA[3]>>2);
+            print_word("\rY-axis: ");
+            print_num(A_DATA);
+            print_char('\n');
+            A_DATA = (uint16_t)(RAW_DATA[4]<<8) | (uint16_t)(RAW_DATA[5]>>2);
+            print_word("\rZ-axis: ");
+            print_num(A_DATA);
+            print_char('\n');
+        }
+        print_word("\rpentla while\n");
+        while(ii)
+            {
+                --ii;
+            }
+        print_word("\rData ready");
+        DataReady = 1;
+        ii = 0xbffff;
+        
+    }
+    */
 };
 
 
 void PORTA_IRQHandler()
 {
-	PORTA_PCR14 |= PORT_PCR_ISF_MASK;			// Clear the interrupt flag 
-	DataReady = 1;	
-}
-
+    PORTA_PCR14 |= PORT_PCR_ISF_MASK;           // Clear the interrupt flag 
+    print_word("\rInterroupt");
+    DataReady = 1;  
