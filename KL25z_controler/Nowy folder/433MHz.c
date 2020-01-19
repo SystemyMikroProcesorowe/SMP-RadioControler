d4052afd782382754a54d75f816fd52278c9b4c9
wisnia1998 <48693427+wisnia1998@users.noreply.github.com>
2020-01-19 20:50:55 +0100

    do sprawdzenia
    trzeba skiończyć komunikacje

--- KL25z_controler/Nowy folder/433MHz.c
+++ KL25z_controler/Nowy folder/433MHz.c
@@ -35,7 +35,7 @@ void set_FR_power(uint16_t power){
 void set_FR_dir(uint8_t dir){
 		FR_dir = dir;
 }
-void calculate_motor_power(uint8_t x_axis, uint8_t x_dir, uint8_t y_axis, uint8_t y_dir){	
+void calculate_motor_power(){//uint8_t x_axis, uint8_t x_dir, uint8_t y_axis, uint8_t y_dir){	
 	L_motor_power = R_motor_power = FR_power;
 	if(LR_dir == 0){
 		L_motor_power -= LR_power;
@@ -59,8 +59,53 @@ void calculate_motor_power(uint8_t x_axis, uint8_t x_dir, uint8_t y_axis, uint8_
 	}
 }
 
-void prepare(){
+uint8_t get_L_motor_power(){
+	return L_motor_power;
+}
+uint8_t get_R_motor_power(){
+	return R_motor_power;
+}
+
+uint8_t get_L_motor_dir(){
+	return L_motor_dir;
+}
+uint8_t get_R_motor_dir(){
+	return R_motor_dir;
+}
+
+void prepare_data(){
 	set_FR_power(X_axis());
 	set_LR_power(Y_axis());
+	calculate_motor_power();
+}
+
+void prepare_data_frame(){
+	tx_buff[0] = 1;
+	tx_buff[1] = 1;
+	tx_buff[2] = 1;
+	tx_buff[3] = 1;
+	tx_buff[4] = 1;
+	tx_buff[5] = 0;
+	tx_buff[6] = 1;
+	tx_buff[7] = 1;		//start frame
+	tx_buff[8] = get_L_motor_dir();		//L_dir bit
+	tx_buff[9] = 0;		///////////	
+	tx_buff[10] = 0;
+	tx_buff[11] = 0;
+	tx_buff[12] = 0;	//data
+	tx_buff[13] = 0;
+	tx_buff[14] = 0;
+	tx_buff[15] = 0;	/////////
+	tx_buff[16] = get_R_motor_dir();	//R_dir bit
+	tx_buff[17] = 0;	/////////
+	tx_buff[18] = 0;
+	tx_buff[19] = 0;
+	tx_buff[20] = 0;	//data
+	tx_buff[21] = 0;
+	tx_buff[22] = 0;
+	tx_buff[23] = 0;	/////////
+}
+
+void transmit(){
 	
 }
--- KL25z_controler/Nowy folder/433MHz.h
+++ KL25z_controler/Nowy folder/433MHz.h
@@ -1,6 +1,8 @@
 #include "MKL25Z4.h"                    // Device header
 
-uint8_t tx_buff[16] = {};
+uint8_t strat_frame = 0b11111011;
+uint8_t current_bit = 0;
+uint8_t tx_buff[24] = {};
 uint8_t LR_power = 0;
 uint8_t LR_dir = 0;				//L=0, R=1
 uint8_t FR_power = 0;
@@ -20,10 +22,13 @@ uint8_t get_FR_dir();
 void set_FR_power(uint16_t power);
 void set_FR_dir(uint8_t dir);
 /*engin power*/
-void calcuate_motor_power(uint8_t x_axis, uint8_t x_dir, uint8_t y_axis, uint8_t y_dir);
+void calcuate_motor_power();//uint8_t x_axis, uint8_t x_dir, uint8_t y_axis, uint8_t y_dir);
 uint8_t get_L_motor_power();
 uint8_t get_R_motor_power();
+uint8_t get_L_motor_dir();
+uint8_t get_R_motor_dir();
 /*transmision*/	
-void prepare();
+void prepare_data();
+void prepare_data_frame();
 void transmit();
 	
