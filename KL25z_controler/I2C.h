5ad2fa78b8b45f55e146d20d3463bbeda54eec58
Tomasz Łochmańczyk <tomek.lochmanczyk@gmail.com>
2020-01-27 00:25:00 +0100

    Update I2C.h

--- KL25z_controler/I2C.h
+++ KL25z_controler/I2C.h
@@ -1,6 +1,5 @@
 #include "MKL25Z4.h"                    // Device header
 
-//I2C function
 void I2C_init();			/*I2C initialization function*/
 void I2C_write(uint8_t SlaveAddr, uint8_t RegAddr, uint8_t data);
 uint8_t I2C_read(uint8_t SlaveAddr, uint8_t RegAddr);
