876a7754a3569ca585d0d01016d2a07a13f61057
Tomasz Łochmańczyk <tomek.lochmanczyk@gmail.com>
2020-01-27 00:28:34 +0100

    Update MMA8451Q.h

--- KL25z_controler/MMA8451Q.h
+++ KL25z_controler/MMA8451Q.h
@@ -15,29 +15,30 @@
 #define WHO_AM_I 0x0D
 #define TRIG_CFG 0x0A
 #define SYSMOD	 0x0B
-#define INT_SOURCE 		0x0C*/
+#define INT_SOURCE 	0x0C
 #define XYZ_DATA_CFG	0x0E
-#define PL_CFG 				0x11
-#define PL_COUNT 			0x12
+#define PL_CFG 		0x11
+#define PL_COUNT 	0x12
 #define PL_BF_ZCOMP 	0x13
-#define P_L_THS_REG 				0x14
-#define FF_MT_CFG 					0x15
-#define FF_MT_SRC						0x16
-#define FF_MT_THS 					0x17
-#define FF_MT_COUNT 				0x18*/
-#define CTRL_REG1 					0x2A
-#define CTRL_REG2 					0x2B
-#define CTRL_REG3 					0x2C
-#define CTRL_REG4 					0x2D
-#define CTRL_REG5 					0x2E
-#define ASLP_COUNT_REG      0x29
+#define P_L_THS_REG 	0x14
+#define FF_MT_CFG 	0x15
+#define FF_MT_S		0x16
+#define FF_MT_THS 	0x17
+#define FF_MT_COUNT 	0x18
+#define CTRL_REG1 	0x2A
+#define CTRL_REG2 	0x2B
+#define CTRL_REG3 	0x2C
+#define CTRL_REG4 	0x2D
+#define CTRL_REG5 	    0x2E
+#define ASLP_COUNT_REG      0x29 
+ 
 #define OFF_Y_REG             0x30
 #define OFF_Z_REG             0x31
 #define TRANSIENT_CFG_REG     0x1D    	// TRANSIENT_CFG Transient Configuration Register 
 #define TRANSIENT_SRC_REG     0x1E    	// TRANSIENT_SRC Transient Source Register 
 #define TRANSIENT_THS_REG     0x1F    	// TRANSIENT_THS Transient Threshold Register 
 #define TRANSIENT_COUNT_REG   0x20    	// TRANSIENT_COUNT Transient Debounce Counter Register
-#define SENSITIVITY_2G		  4096
+#define SENSITIVITY_2G	      4096
 	
 	
 void ACCEL_init(void);
